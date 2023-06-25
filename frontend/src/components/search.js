20fb0db401ef2f9a9113fad853f68ecf952d3e9e
tusharpasricha <tusharpasricha21@gmail.com>
2023-06-25 12:28:54 +0530

    search bar design

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -68,6 +68,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import "./search.css"
 
 const Search = ({ setIsTransaction ,setSearchR }) => { // history is a prop provided by react-router-dom
   const [searchQuery, setSearchQuery] = useState('');
@@ -115,13 +116,25 @@ const Search = ({ setIsTransaction ,setSearchR }) => { // history is a prop prov
   return (
     <>
       <div className='searchbar'>
-        <input
-          placeholder='Search by Block Height, Block Hash or Transaction Id'
-          type='text'
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
+        <div class="search">
+        <div class="search-box">
+          <div class="search-field">
+            <input placeholder='Search by Block Height, Block Hash or Transaction Id'
+            type='text'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)} class="input" 
+          />
+            <div class="search-box-icon">
+              <button onClick={handleSearch} class="btn-icon-content">
+                <i class="search-icon">
+                  <svg xmlns="://www.w3.org/2000/svg" version="1.1" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" fill="#fff"></path></svg>
+                </i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+        {/* <button >Search</button> */}
       </div>
       
     </>
