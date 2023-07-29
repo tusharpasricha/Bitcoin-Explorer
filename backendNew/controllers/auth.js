fb2ca640fb8f8f4a864334a8cee1200369dad63f
tusharpasricha <tusharpasricha21@gmail.com>
2023-07-29 12:55:15 +0530

    fixing server error

--- backendNew/controllers/auth.js
+++ backendNew/controllers/auth.js
@@ -3,21 +3,21 @@ const Comment = require('../models/Comment')
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const {
-   createJWT,
+  createJWT,
 } = require("../utils/auth");
 const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-exports.comments = (req,res,next) =>{
-  let {comment, email, transaction } = req.body;
+exports.comments = (req, res, next) => {
+  let { comment, email, transaction } = req.body;
   let errors = [];
-  if(!comment){
-    errors.push({comment:"required"});
+  if (!comment) {
+    errors.push({ comment: "required" });
   }
-  if(!email){
-    errors.push({name:"required"})
+  if (!email) {
+    errors.push({ name: "required" })
   }
-  if(!transaction){
-    errors.push({transaction:"required"})
+  if (!transaction) {
+    errors.push({ transaction: "required" })
   }
   const userComment = new Comment({
     comment: comment,
@@ -25,17 +25,17 @@ exports.comments = (req,res,next) =>{
     transaction: transaction,
   });
   userComment.save()
-             .then(response => {
-                res.status(200).json({
-                  success: true,
-                  result: response
-                })
-             })
-             .catch(err => {
-               res.status(500).json({
-                  errors: [{ error: err }]
-               });
-            });
+    .then(response => {
+      res.status(200).json({
+        success: true,
+        result: response
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        errors: [{ error: err }]
+      });
+    });
 
 }
 
@@ -45,7 +45,7 @@ exports.getAllComments = (req, res, next) => {
   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  Comment.find({transaction:transactionId})
+  Comment.find({ transaction: transactionId })
     .then(comments => {
       res.status(200).json({
         success: true,
@@ -77,7 +77,7 @@ exports.signup = (req, res, next) => {
   }
   if (!password_confirmation) {
     errors.push({
-     password_confirmation: "required",
+      password_confirmation: "required",
     });
   }
   if (password != password_confirmation) {
@@ -86,90 +86,94 @@ exports.signup = (req, res, next) => {
   if (errors.length > 0) {
     return res.status(422).json({ errors: errors });
   }
- User.findOne({email: email})
-    .then(user=>{
-       if(user){
-          return res.status(422).json({ errors: [{ user: "email already exists" }] });
-       }else {
-         const user = new User({
-           name: name,
-           email: email,
-           password: password,
-         });
- bcrypt.genSalt(10, function(err, salt) { bcrypt.hash(password, salt, function(err, hash) {
-         if (err) throw err;
-         user.password = hash;
-         user.save()
-             .then(response => {
+  User.findOne({ email: email })
+    .then(user => {
+      if (user) {
+        return res.status(422).json({ errors: [{ user: "email already exists" }] });
+      } else {
+        const user = new User({
+          name: name,
+          email: email,
+          password: password,
+        });
+        bcrypt.genSalt(10, function (err, salt) {
+          bcrypt.hash(password, salt, function (err, hash) {
+            if (err) throw err;
+            user.password = hash;
+            user.save()
+              .then(response => {
                 res.status(200).json({
                   success: true,
                   result: response
                 })
-             })
-             .catch(err => {
-               res.status(500).json({
+              })
+              .catch(err => {
+                res.status(500).json({
                   errors: [{ error: err }]
-               });
-            });
-         });
-      });
-     }
-  }).catch(err =>{
+                });
+              });
+          });
+        });
+      }
+    }).catch(err => {
       res.status(500).json({
         errors: [{ error: 'Something went wrong' }]
       });
-  })
+    })
 }
 exports.signin = (req, res) => {
-     let { email, password } = req.body;
-     let errors = [];
-     if (!email) {
-       errors.push({ email: "required" });
-     }
-     if (!emailRegexp.test(email)) {
-       errors.push({ email: "invalid email" });
-     }
-     if (!password) {
-       errors.push({ passowrd: "required" });
-     }
-     if (errors.length > 0) {
-      return res.status(422).json({ errors: errors });
-     }
-     User.findOne({ email: email }).then(user => {
-        if (!user) {
-          return res.status(404).json({
-            errors: [{ user: "not found" }],
+  let { email, password } = req.body;
+  let errors = [];
+  if (!email) {
+    errors.push({ email: "required" });
+  }
+  if (!emailRegexp.test(email)) {
+    errors.push({ email: "invalid email" });
+  }
+  if (!password) {
+    errors.push({ password: "required" });
+  }
+  if (errors.length > 0) {
+    return res.status(422).json({ errors: errors });
+  }
+  User.findOne({ email: email }).then(user => {
+    if (!user) {
+      return res.status(404).json({
+        errors: [{ user: "not found" }],
+      });
+    } else {
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (!isMatch) {
+          return res.status(400).json({
+            errors: [{
+              password:
+                "incorrect"
+            }]
           });
-        } else {
-           bcrypt.compare(password, user.password).then(isMatch => {
-              if (!isMatch) {
-               return res.status(400).json({ errors: [{ password:
-"incorrect" }] 
-               });
-              }
-       let access_token = createJWT(
+        }
+        let access_token = createJWT(
           user.email,
           user._id,
           3600
-       );
-       jwt.verify(access_token, process.env.TOKEN_SECRET, (err,
-decoded) => {
-         if (err) {
-            res.status(500).json({ erros: err });
-         }
-         if (decoded) {
-             return res.status(200).json({
-                success: true,
-                token: access_token,
-                message: user
-             });
-           }
-         });
-        }).catch(err => {
-          res.status(500).json({ erros: err });
+        );
+        jwt.verify(access_token, "tusharpasricha", (err,
+          decoded) => {
+          if (err) {
+            return res.status(500).json({ erros: err });
+          }
+          if (decoded) {
+            return res.status(200).json({
+              success: true,
+              token: access_token,
+              message: user
+            });
+          }
         });
-      }
-   }).catch(err => {
-      res.status(500).json({ erros: err });
-   });
+      }).catch(err => {
+        return res.status(500).json({ erros: err });
+      });
+    }
+  }).catch(err => {
+    return res.status(500).json({ erros: err });
+  });
 }
