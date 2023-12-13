12e2e2105b79ef2fb0968ca9d0f4b7415f4a65db
Tushar Pasricha <tusharpasricha@Tushars-MacBook-Air-2.local>
2023-12-13 10:46:37 +0530

    / end point

--- backendNew/src/routes/auth.js
+++ backendNew/src/routes/auth.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 const { signup, signin, comments,getAllComments } = require('../controllers/auth');
 
+router.get('/',(req,res)=>{
+    res.status(200).json({
+    success: true,
+    result: "working"
+  })})
+
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/comments', comments);
